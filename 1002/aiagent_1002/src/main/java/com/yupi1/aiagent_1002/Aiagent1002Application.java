32af12bfa8fa22b19517145bd389a8c9e43c0eab
midnightzz1 <2780472612@qq.com>
2025-10-02 19:56:55 +0800

    init

---
+++ 1002/aiagent_1002/src/main/java/com/yupi1/aiagent_1002/Aiagent1002Application.java
@@ -0,0 +1,13 @@
+package com.yupi1.aiagent_1002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aiagent1002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Aiagent1002Application.class, args);
+    }
+
+}
---
+++ 1002/aiagent_1002/src/test/java/com/yupi1/aiagent_1002/Aiagent1002ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yupi1.aiagent_1002;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aiagent1002ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
